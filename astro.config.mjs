a8ed1a879c363b75994e291bdcbf445f7797118b
wendy7756 <WENDY7756@OUTLOOK.COM>
2025-08-11 23:18:01 +0800

    Update domain URLs to resumelink.cloud

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ResumeLink.com', // 替换为你的实际域名
+  site: 'https://resumelink.cloud', // 实际域名
   
   // 启用压缩
   compressHTML: true,
